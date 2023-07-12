354af6a5152cb6a5cbd11a276949d3ddb76b9db1
Loay Alhamwi <loay.hamwi@gmail.com>
2023-07-12 12:41:38 +0300

    Update and rename Untitled.py to readfilestitles.py

--- readfilestitles.py
+++ readfilestitles.py
@@ -1,9 +1,6 @@
+# the purpose of this code is to read the file titles from path and generate data frame then get the course files in specific format
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[ ]:
-
-
 data=list()
 import pandas as pd
 import os
@@ -12,52 +9,15 @@ for (root,dirs,file) in os.walk(path):
     for x in file:
         #print(x)
         data.append(x)
-        
+      
 df1=pd.DataFrame(data)
-
-
-# In[ ]:
-
-
 print(df1)
-
-
-# In[ ]:
-
-
 df1[0]= df1[0].str.replace('ArchiveFile_','')
 df1[0]= df1[0].str.replace('.zip','')
-
-
-# In[ ]:
-
-
 df1['EXTERNAL_COURSE_KEY']=''
 df1['AVAILABLE_IND']='N'
 df1['ROW_STATUS']='enabled'
-
-
-# In[ ]:
-
-
 df1.columns=['COURSE_ID','EXTERNAL_COURSE_KEY','AVAILABLE_IND','ROW_STATUS']
 df1['EXTERNAL_COURSE_KEY']=df1['COURSE_ID'].str[-6:]+'.'+df1['COURSE_ID'].str[-12:-7]
-
-
-# In[ ]:
-
-
 df1
-
-
-# In[ ]:
-
-
 df1.to_csv('mylist.csv', index=False)
-
-
-# In[ ]:
-
-
-
-
